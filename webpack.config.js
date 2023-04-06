b121daf0f97fa47fb2830940f5c47e388e3b2655
Валерий Булаш <vbulash@yandex.ru>
2023-04-06 23:19:52 +0300

    Финальная версия wepack

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	resolve: {
-		alias: {
-			'@': path.resolve('/resources/js')
-		},
-		// No effect
-		// extensions: [".*", ".wasm", ".mjs", ".js", ".jsx", ".json"]
-	}
-};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,17 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
+// Конфигурация webpack
+mix.webpackConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './resources/js')
+		},
+		extensions: ['.jsx']
+	}
+});
+
+// Директивы сборки
 mix
 	.js('resources/js/app.jsx', 'public/js')
 	.react()
